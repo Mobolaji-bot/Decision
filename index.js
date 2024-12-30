ed2a3dae28ad2ca0a5b7d11a6638d9304f8d740a
mobolaji oko-osi <mmobolaji16@gmail.com>
2024-12-30 21:29:38 +0100

    decision

---
+++ index.js
@@ -0,0 +1,21 @@
+let age = 12
+
+if (age === 12) {
+    return "$10 per ticket"
+} else if (age >=13 && age <= 17 ) {
+    return "$15 per ticket"
+}
+else {
+    return "$20 per ticket"
+}
+
+let weather = hot
+if (weather === hot) {
+    return "wear something light"
+} else if (weather === warm) {
+    return "wear anything"
+}
+else{
+    return "its cold"
+}
+
